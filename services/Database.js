ba74b17af4203ee8cdadb68ba9eb579b1e5ed883
belorin <valeriociccio@yahoo.it>
2022-12-20 14:18:14 +0100

    aggiungo classe database

---
+++ services/Database.js
@@ -0,0 +1,23 @@
+class Database {
+    mysql
+    connection
+    constructor() {
+        this.mysql = require('mysql2');
+        this.connection = this.mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'tirocinio_2022_2023',
+        });
+    }
+    query = (query) => {
+        let answer
+        let error
+        this.connection.query(query, (err, results, fields) => {
+            answer = results
+            error = err
+        })
+        if(answer) return answer
+        return error
+    }
+}
+export default new Database();
