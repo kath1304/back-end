d735d9bfbd99dedcdf472add33358e02f6f0fcc2
belorin <valeriociccio@yahoo.it>
2022-12-20 17:13:54 +0100

    aggiungo logged session

--- object/LoggedSession.js
+++ object/LoggedSession.js
@@ -1,18 +1,51 @@
+import express from "express"
+import {database} from "../services/Database.js"
+
 class LoggedSession {
-    ipAddress
-    username
-    accessDate
+    ip_address
+    user_username
+    access_date
     constructor(ip,username,date){
-        this.ipAddress = ip
-        this.username = username
-        this.accessDate = date
+        this.ip_address = ip
+        this.user_username = username
+        this.access_date = date
     }
-    get username(){return this.username}
-    set username(username) {this.username = username}
-    get ipAddress(){return this.ipAddress}
-    set ipAddress(ip) {this.ipAddress = ip}
-    get accessDate() {return this.accessDate}
-    set accessDate(date) {this.accessDate = date}
+    get username(){return this.user_username}
+    set username(username) {this.user_username = username}
+    get ipAddress(){return this.ip_address}
+    set ipAddress(ip) {this.ip_address = ip}
+    get accessDate() {return this.access_date}
+    set accessDate(date) {this.access_date = date}
+
+    async getByIp(ip) {
+        let result = await database.request('SELECT * FROM `logged_session` WHERE `ip_address` = ip')
+        console.log(result)
+    }
+    async getByUsername(username) {
+        let result = database.request('SELECT * FROM `logged_session` WHERE `user_username` = ip')
+        console.log(result)
+    }
+    async save(){
+        let result = await database.request('INSERT INTO logged_session SET ?', this)
+        console.log(result)
+    }
+    async delete(){
+        let result = await database.request('DELETE FROM `logged_session` WHERE `ip_address` = this.ip_address')
+        console.log(result)
+    }
+    async deleteByUsername(username){
+        let result = await database.request('DELETE FROM `logged_session` WHERE `username` = username')
+        console.log(result)
+    }
+    async deleteByIp(ip){
+        let result = await database.request('DELETE FROM `logged_session` WHERE `ip_address` = ip')
+        console.log(result)
+    }
+    //specificare il cascade all in phpstorm
+    async update(loggedSession){
+        let result = await database.request('UPDATE `logged_session` SET ? WHERE `ip_address` = this.ip_address', loggedSession)
+        console.log(result)
+    }
 }
 
 export {LoggedSession}
