80fde2de710bf31a1a0886db19fb4c838db2144e
belorin <valeriociccio@yahoo.it>
2022-12-22 11:10:36 +0100

    modifico loggedSession

--- object/LoggedSession.js
+++ object/LoggedSession.js
@@ -45,8 +45,15 @@ class LoggedSession {
     }
     //specificare il cascade all in phpstorm
     async update(loggedSession){
-        let result = await database.request('UPDATE `logged_session` SET ? WHERE `ip_address` = this.ip_address', loggedSession)
+        let result
+        try {
+            result = await database.request('UPDATE `logged_session` SET ? WHERE `ip_address` = this.ip_address', loggedSession)
+        }
+        catch (e) {
+            console.error(e)
+        }
         console.log(result)
+        return result
     }
 }
 
