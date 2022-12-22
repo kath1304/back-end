72d6319c86ee95795230daf405502705e0e0315d
kath1304 <cat.cretoni@stud.uniroma3.it>
2022-12-22 11:11:07 +0100

    aggiornato User con try-catch

--- object/user.js
+++ object/user.js
@@ -1,16 +1,13 @@
 import {database} from "../services/Database";
 
-export class User
-
-{
+export class User {
     _username;
     _name;
     _surname;
     _email;
 
 
-    constructor(username, name, surname, email)
-    {
+    constructor(username, name, surname, email) {
         this._username = username;
         this._name = name;
         this._surname = surname;
@@ -18,94 +15,93 @@ export class User
 
 
     }
- //GETTER AND SETTER
+
+    //GETTER AND SETTER
 
     //USERNAME
-    get
-    username()
-    {
+    get username() {
         return this._username;
     }
-    set
-    username(username)
-    {
+
+    set username(username) {
         this._username = name;
     }
 
     //NAME
-    get
-    name()
-    {
+    get name() {
         return this._name;
     }
-    set
-    name(name)
-    {
+
+    set name(name) {
         this._name = name;
     }
 
     //SURNAME
 
-    get
-    surname()
-    {
+    get surname() {
         return this._surname;
     }
-    set
-    surname(surname)
-    {
+
+    set surname(surname) {
         this._surname = surname;
     }
 
     //EMAIL
 
-    get
-    email()
-    {
+    get email() {
         return this._email;
     }
-    set
-    email(email)
-    {
+
+    set email(email) {
         this._email = email;
     }
 
     //READ METHOD BY USERNAME --> RETURN COMPLETE USER OBJ
-   async  getByUsername(nickname){
-        let result= await database.request('SELECT * FROM `user` WHERE `username`=nickname');
-        console.log(result);
+    async getByUsername(nickname) {
+        let result
+
+        try {
+            result = await database.request('SELECT * FROM `user` WHERE `username`=nickname');
+            console.log(result);
+        } catch (error) {
+            console.error(error)
+        }
+
     }
 
     //SAVE METHOD  --> PUT THE NEW USER INTO THE DB
-    async  save(){
-        let result= await database.request('INSERT INTO user SET ?',this);
-        console.log(result);
+    async save() {
+        let result
+        try {
+            result = await database.request('INSERT INTO user SET ?', this);
+            console.log(result);
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     //DELETE METHOD BY USERNAME --> RETURN THE USER OBJ DELETED
-    async  deleteByUserName(nickname){
-        let result= await database.request('DELETE FROM user WHERE `username`=nickname');
-        console.log(result);
+    async deleteByUserName(nickname) {
+        let result
+        try {
+            result = await database.request('DELETE FROM user WHERE `username`=nickname');
+            console.log(result);
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     //UPDATE METHOD BY USER --> UPLOAD THE REFRESH USER INTO THE DB
     //OSS: CASCADE
-    async  upDateByUserName(user){
-        let result= await database.request('UPDATE user SET ? WHERE `username`=this.username',user);
-        console.log(result);
+    async upDateByUserName(user) {
+        let result
+        try {
+            result = await database.request('UPDATE user SET ? WHERE `username`=this.username', user);
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
