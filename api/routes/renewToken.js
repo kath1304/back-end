a443cfc9ac7379f0c63d8f8deb3fe9f566420d28
belorin <valeriociccio@yahoo.it>
2023-01-12 11:12:11 +0100

    migliorato renew del pacchetto jwt

--- api/routes/renewToken.js
+++ api/routes/renewToken.js
@@ -4,12 +4,15 @@ import express from 'express'
 const renewToken = express.Router()
 let webToken = new WebToken()
 
-renewToken.get('/newToken', (req, res, next) => {
+renewToken.get('/', (req, res, next) => {
     let token = req.headers["authorization"].split(" ")[1]
     let data = webToken.validate(token)
     if(data) {
         let newToken = webToken.generate(data.username, data.role)
-        return res.json(newToken)
+        return res.json({
+            token: newToken,
+            role: data.role
+        })
     }
     const error = new Error("Token not valid")
     error.status = 403
